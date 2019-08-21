8218bce35a952dae3dcac7b4174956462fc04b00
metagento <support@metagento.com>
2019-08-22 00:34:43 +0700

    Fix compatible issues with Magento 2.3

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -31,59 +31,84 @@ class InstallSchema implements
          * Create table 'newsletterdiscountpro_program'
          */
         $table = $installer->getConnection()
-                           ->newTable($installer->getTable('newsletterdiscountpro_program'))
-                           ->addColumn(
-                               'program_id', Table::TYPE_INTEGER, null,
-                               ['identity' => true, 'nullable' => false, 'primary' => true, 'auto_increment' => true]
-                           )
-                           ->addColumn(
-                               'from_date', Table::TYPE_DATETIME, null,
-                               ['nullable' => true]
-                           )
-                           ->addColumn(
-                               'to_date', Table::TYPE_DATETIME, null,
-                               ['nullable' => true]
-                           )
-                           ->addColumn(
-                               'number_order', Table::TYPE_INTEGER, null,
-                               ['nullable' => false, 'default' => 0]
-                           )
-                           ->addColumn(
-                               'use_coupon', Table::TYPE_INTEGER, 3,
-                               ['nullable' => false, 'default' => 0]
-                           )
-                           ->addColumn(
-                               'coupon_code', Table::TYPE_TEXT, null,
-                               ['nullable' => false, 'default' => '']
-                           )
-                           ->addColumn(
-                               'discount_type', Table::TYPE_TEXT, 100,
-                               ['nullable' => false, 'default' => '']
-                           )
-                           ->addColumn(
-                               'discount_value', Table::TYPE_DECIMAL, null,
-                               ['nullable' => false, 'default' => 0]
-                           )
-                           ->addColumn(
-                               'discount_on', Table::TYPE_TEXT, 100,
-                               ['nullable' => false, 'default' => '']
-                           )
-                           ->addColumn(
-                               'created_at', Table::TYPE_DATETIME, null,
-                               ['nullable' => false]
-                           )
-                           ->addColumn(
-                               'priority', Table::TYPE_INTEGER, null,
-                               ['nullable' => false]
-                           );
+            ->newTable($installer->getTable('newsletterdiscountpro_program'))
+            ->addColumn(
+                'program_id', Table::TYPE_INTEGER, null,
+                ['identity' => true, 'nullable' => false, 'primary' => true, 'auto_increment' => true],
+                "Program Id"
+            )
+            ->addColumn(
+                'from_date', Table::TYPE_DATETIME, null,
+                ['nullable' => true],
+                "From Date"
+            )
+            ->addColumn(
+                'to_date', Table::TYPE_DATETIME, null,
+                ['nullable' => true],
+                "To Date"
+            )
+            ->addColumn(
+                'number_order', Table::TYPE_INTEGER, null,
+                ['nullable' => false, 'default' => 0],
+                "Number Order"
+            )
+            ->addColumn(
+                'use_coupon', Table::TYPE_INTEGER, 3,
+                ['nullable' => false, 'default' => 0],
+                "User Coupon"
+            )
+            ->addColumn(
+                'coupon_code', Table::TYPE_TEXT, null,
+                ['nullable' => false, 'default' => ''],
+                "Coupon Code"
+            )
+            ->addColumn(
+                'discount_type', Table::TYPE_TEXT, 100,
+                ['nullable' => false, 'default' => ''],
+                "Discount Type"
+            )
+            ->addColumn(
+                'discount_value', Table::TYPE_DECIMAL, null,
+                ['nullable' => false, 'default' => 0],
+                "Discount Value"
+            )
+            ->addColumn(
+                'discount_on', Table::TYPE_TEXT, 100,
+                ['nullable' => false, 'default' => ''],
+                "Discount On"
+            )
+            ->addColumn(
+                'created_at', Table::TYPE_DATETIME, null,
+                ['nullable' => false],
+                "Created At"
+            )
+            ->addColumn(
+                'priority', Table::TYPE_INTEGER, null,
+                ['nullable' => false],
+                "Priority"
+            );
         $installer->getConnection()->createTable($table);
 
-        $installer->getConnection()->addColumn($setup->getTable('sales_order'), 'newsletter_discount', 'DECIMAL(10,2) NOT NULL DEFAULT 0');
-        $installer->getConnection()->addColumn($setup->getTable('sales_order'), 'base_newsletter_discount', 'DECIMAL(10,2) NOT NULL DEFAULT 0');
-        $installer->getConnection()->addColumn($setup->getTable('sales_invoice'), 'newsletter_discount', 'DECIMAL(10,2) NOT NULL DEFAULT 0');
-        $installer->getConnection()->addColumn($setup->getTable('sales_invoice'), 'base_newsletter_discount', 'DECIMAL(10,2) NOT NULL DEFAULT 0');
-        $installer->getConnection()->addColumn($setup->getTable('sales_creditmemo'), 'newsletter_discount', 'DECIMAL(10,2) NOT NULL DEFAULT 0');
-        $installer->getConnection()->addColumn($setup->getTable('sales_creditmemo'), 'base_newsletter_discount', 'DECIMAL(10,2) NOT NULL DEFAULT 0');
+        $columnDefination = [
+            "type"     => \Magento\Framework\DB\Ddl\Table::TYPE_DECIMAL,
+            "length"   => "12,4",
+            "nullable" => false,
+            "default"  => 0,
+            "comment"  => "Newsletter Discount"
+        ];
+
+        $installer->getConnection()->addColumn($setup->getTable('sales_order'), 'newsletter_discount',
+            $columnDefination);
+        $installer->getConnection()->addColumn($setup->getTable('sales_order'), 'base_newsletter_discount',
+            $columnDefination);
+        $installer->getConnection()->addColumn($setup->getTable('sales_invoice'), 'newsletter_discount',
+            $columnDefination);
+        $installer->getConnection()->addColumn($setup->getTable('sales_invoice'), 'base_newsletter_discount',
+            $columnDefination);
+        $installer->getConnection()->addColumn($setup->getTable('sales_creditmemo'), 'newsletter_discount',
+            $columnDefination);
+        $installer->getConnection()->addColumn($setup->getTable('sales_creditmemo'), 'base_newsletter_discount',
+            $columnDefination);
 
         $installer->endSetup();
         return;
